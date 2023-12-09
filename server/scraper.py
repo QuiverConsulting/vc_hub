bc57cf4ddc17ddf11f26d380fc2504a60e15bb80
ariannedjabat <ariannedjabat@gmail.com>
2023-12-09 18:47:58 -0500

    copy postman suggestion

--- server/scraper.py
+++ server/scraper.py
@@ -14,8 +14,16 @@ sites = [
 
 def scrape():
     for site in sites:
-        page = requests.get(site, headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"})
-        print(page.content)
+        # page = requests.get(site, headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"})
+        # print(page.content)
+        url = "https://techcrunch.com/category/venture/"
+
+        payload = {}
+        headers = {}
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        print(response.text)
 
 if __name__ == '__main__':
     scrape()
