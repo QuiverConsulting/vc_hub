44b192d64de4d5ee0e5df2caaa0900dec79f792f
ariannedjabat <ariannedjabat@gmail.com>
2023-12-09 18:44:12 -0500

    add user agent

--- server/scraper.py
+++ server/scraper.py
@@ -2,20 +2,20 @@ import logging
 import requests
 
 sites = [
-    'https://www.geekwire.com/fundings/'
+    # 'https://www.geekwire.com/fundings/'
     #'https://techcrunch.com/category/startups/',
-    #'https://techcrunch.com/category/venture/',
+    'https://techcrunch.com/category/venture/',
     #'https://news.crunchbase.com/',
-    #'https://news.crunchbase.com/sections/seed/',
-    #'https://www.eu-startups.com/category/fundin/',
-    #'https://sifted.eu/sector/venture-capital',
+    # 'https://news.crunchbase.com/sections/seed/',
+    # 'https://www.eu-startups.com/category/fundin/',
+    # 'https://sifted.eu/sector/venture-capital',
     #'https://www.finsmes.com/'
 ]
 
 def scrape():
     for site in sites:
-        page = requests.get(site)
-        print(page.text)
+        page = requests.get(site, headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"})
+        print(page.content)
 
 if __name__ == '__main__':
     scrape()
