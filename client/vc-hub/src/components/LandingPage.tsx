a986ca22d1b26a2194783af5fdaef8005bd3b1d6
Silina George <19698115+SilinaGeorge@users.noreply.github.com>
2024-04-29 18:13:45 -0400

    landing page component

---
+++ client/vc-hub/src/components/LandingPage.tsx
@@ -0,0 +1,76 @@
+
+import Table from "./table/Table";
+import { useRef } from "react";
+import { Link as ScrollLink } from 'react-scroll';
+import { BouncyDownArrow } from "./BouncyDownArrow";
+import styled from "styled-components";
+
+
+
+const TableWrapper = styled("div")(({ theme }) => ({
+  margin: "10vh 5vw 5vh 5vw",
+  height: '80vh',
+  alignContent: 'center'
+}));
+
+const MissionStatementWrapper = styled("div")(({ theme }) => ({
+  textAlign: 'center',
+  ".bold":{
+    fontSize: '65px'
+  },
+  ".normal":{
+    fontSize: '20px'
+  },
+}));
+
+
+const ArrowWrapper = styled('div')`
+  position: absolute;
+  bottom: 3vh;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
+
+const ContentWrapper = styled("div")(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  height: '95vh'
+
+}));
+
+const LandingPage = () => {
+
+  const tableRef = useRef<HTMLDivElement>(null);
+  const scrollToTable = () => {
+    if (tableRef.current) {
+      tableRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <>
+    <ContentWrapper>
+    <MissionStatementWrapper>
+        <p className="bold">
+            Empowering careers by connecting<br /> you with the latest innovations
+        </p>
+        <p className="normal">
+            Discover your next opportunity with companies <br /> fuelled by venture capital at VC Hub.
+        </p>
+    </MissionStatementWrapper>
+    <ScrollLink to="table" spy={true} smooth={true} duration={500} className="arrow">
+        <ArrowWrapper>
+            <BouncyDownArrow onClick={scrollToTable} />
+        </ArrowWrapper>
+    </ScrollLink>
+    </ContentWrapper>
+    <TableWrapper ref={tableRef}>
+        <Table />
+    </TableWrapper></>
+  );
+};
+
+export default LandingPage;
--- client/vc-hub/src/components/Main.tsx
+++ client/vc-hub/src/components/Main.tsx
@@ -1,53 +1,15 @@
-import { ThemeProvider, styled } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import Table from "./table/Table";
 import Header from "./Header";
-import { useEffect, useState, useRef } from "react";
-
-import { Link as ScrollLink } from 'react-scroll';
-import { BouncyDownArrow } from "./BouncyDownArrow";
+import { useEffect, useState } from "react";
 import { lightTheme, darkTheme } from "../Themes";
+import LandingPage from "./LandingPage";
 
-
-const TableWrapper = styled("div")(({ theme }) => ({
-  margin: "10vh 5vw 5vh 5vw",
-  height: '80vh',
-  alignContent: 'center'
-}));
-
-const MissionStatementWrapper = styled("div")(({ theme }) => ({
-  textAlign: 'center',
-  ".bold":{
-    fontSize: '65px'
-  },
-  ".normal":{
-    fontSize: '20px'
-  },
-}));
-
-
-const ArrowWrapper = styled('div')`
-  position: absolute;
-  bottom: 3vh;
-  left: 50%;
-  transform: translateX(-50%);
-`;
-
-
-const ContentWrapper = styled("div")(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  height: '95vh'
-
-}));
 
 const Main = () => {
   const localIsLightTheme = localStorage.getItem("isLightTheme")
   const [theme, setTheme] = useState(localIsLightTheme ===null || localIsLightTheme ==="true"?lightTheme:darkTheme );
   const [isLightTheme, setIsLightTheme] = useState<boolean>(localIsLightTheme ===null || localIsLightTheme ==="true"?true:false );
-  const tableRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
       localStorage.setItem("isLightTheme", isLightTheme.toString())
@@ -55,36 +17,11 @@ const Main = () => {
   }, [isLightTheme]);
 
 
-
-  const scrollToTable = () => {
-    if (tableRef.current) {
-      tableRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header setIsLightTheme={setIsLightTheme} isLightTheme={isLightTheme}/>
-     
-        <ContentWrapper>
-        <MissionStatementWrapper>
-          <p className="bold">
-            Empowering careers by connecting<br /> you with the latest innovations
-          </p>
-          <p className="normal">
-            Discover your next opportunity with companies <br /> fuelled by venture capital at VC Hub.
-          </p>
-        </MissionStatementWrapper>
-          <ScrollLink to="table" spy={true} smooth={true} duration={500} className="arrow">
-            <ArrowWrapper>
-            <BouncyDownArrow onClick={scrollToTable} />
-            </ArrowWrapper>
-          </ScrollLink>
-        </ContentWrapper>
-        <TableWrapper ref={tableRef}>
-          <Table />
-      </TableWrapper>
+      <LandingPage />
     </ThemeProvider>
   );
 };
